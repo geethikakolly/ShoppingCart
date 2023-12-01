1924a1d04e001998fb0d2f4a08e65a0172eb82cb
sdivi <sailokesh.d@gmail.com>
2023-12-01 16:19:49 -0500

    changed androidx to support design

---
+++ app/src/main/java/eu/tutorials/shoppingcart/MainActivity.kt
@@ -0,0 +1,16 @@
+package eu.tutorials.shoppingcart
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import eu.tutorials.shoppingcart.ui.theme.ShoppingCartTheme
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
